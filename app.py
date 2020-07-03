6dc0d77b151b0cb39f387ae478c53dcc0c5fcaf4
equle <kimyuunseo@gmail.com>
2020-07-03 19:59:06 +0900

    json 파일 업로드 html 파일 보내기

--- app.py
+++ app.py
@@ -1,79 +1,92 @@
 from flask import Flask, request, render_template, redirect, url_for
+import game
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-# @app.route('/hello')
+# @app.route('/hello/')
 # def hello():
-#     return render_template("hello.html")
+#     return 'Hello, World'
+
+#page move
+
+@app.route('/<string:data>')
+def movepage(data):
+    if data == 'loginpage':
+        return render_template('login.html')
+
+    elif data == 'gamepage':
+            return render_template('game.html')
+
+    else :
+        return render_template('index.html')
 
 
-@app.route('/hello/<name>')
-def hello(name):
-    return "hello {}".format(name)
+#'login'
 
-# login.html
-@app.route('/login')
-def login():
-    return render_template("login.html")
-
-# login merhod get, post
-@app.route('/method', methods=['GET', 'POST'])
-def method():
+@app.route('/putinfo', methods=['GET','POST'])
+def putinfo():
+    ID = request.form['ID']
+    PW = request.form['PW']
     if request.method == 'GET':
-        num = request.args["num"]
-        # name = request.args["name"]
-        # num = request.args.get("num")
-        name = request.args.get("name")
-        # num = request.form["num"]
-        # name = request.form["name"]
-        return "GET으로 전달({},{})".format(num, name)
-    else:
-        num = request.form["num"]
-        name = request.form["name"]
-        # num = request.args["num"]
-        # name = request.args.get("name")
-        
-        return "POST로 전달({},{})".format(num, name)
+        return "GET으로 보내진 데이터는 보호되지 않습니다 전달된 데이터({},{})".format(ID, PW)
+    else :
+        with open("static/save.txt","w",encoding='utf-8') as f:
+            f.write("%s,%s" % (ID, PW))
+        return redirect(url_for('getinfo'))
 
-#test 1,2
-@app.route('/test')
-def test1():
-    return 'test1'
+# save file
 
-@app.route('/test/')
-def test2():
-    return 'test2'
+@app.route('/getinfo')
+def getinfo():
+    # 파일 입력
+    with open("static/save.txt", "r", encoding='utf-8') as file:
+        student = file.read().split(',')  # 쉽표로 잘라서 student 에 배열로 저장
+    return '이름 : {}, 번호 : {}'.format(student[0], student[1])
 
-# redirect to naver, duam
-@app.route('/naver')
-def naver():
-    return render_template("naver.html")
+# send data to gameintro.html
+@app.route('/getname', methods=['GET','POST'])
+def gamesave():
+    username = request.form['user']
+    game.set_charact(username)
+    character = game.get_charact()
+    return render_template('gameintro.html', character = character)
 
-@app.route('/kakao')
-def daum():
-    return redirect("https://www.daum.net/")
+## method
+# @app.route('/method',  methods=['GET', 'POST'])
+# def method():
+#     if request.method == 'GET':
+#         return 'GET으로 전송한다.'
+#     else :
+#         num = request.form['num']
+#         name = request.form['name']
+#         printdata = 'POST로 전달된 학번은 {}이고 이름은 {}이다.)'.format(num, name)
+#         return printdata
 
-# error page
-@app.errorhandler(404)
-def page_not_found(error):
-    return "페이지가 없습니다. URL를 확인 하세요", 404
+## URL
+## URL_FOR
+# @app.route('/naver')
+# def daum():
+#     return redirect("https://www.daum.net/")
+#      return render_template('naver.html')
 
-# @app.route('/nopage')
-# def nopage ():
-#     print("404로 보냅니다.")
-#     abort(404)
-#     return "404로 보냅니다."
+# # @app.route('/move/<name>')
+# # def movevar(name):
+# #     if name == 'naver':
+# #         return redirect(url_for('naver'))
+# #     elif name == 'daum':
+# #         return redirect(url_for('daum'))
+# #     else :
+# #         return redirect(url_for('index'))
 
-# pg.html
-@app.route('/pang')
-def pang ():
-    return render_template("pg.html")
 
-if __name__ =='__main__':
-    with app.test_request_context():
-        print(url_for('daum'))
 
+## app.run ##
+if __name__ == '__main__':
+    # with app.test_request_context():
+    #     print(url_for('daum'))
     app.run(debug=True)
